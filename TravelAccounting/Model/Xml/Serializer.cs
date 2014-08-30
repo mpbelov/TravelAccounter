2971b3db979ce1812771a6c9ad4c455194a560c6
Mikhail Belov <mpbelov@gmail.com>
2014-08-30 22:21:39 +0400

    serializer supports currencies

--- TravelAccounting/Model/Xml/Serializer.cs
+++ TravelAccounting/Model/Xml/Serializer.cs
@@ -7,12 +7,9 @@ using System.Xml;
 using System.Xml.Linq;
 using TravelAccounting.Contracts;
 
-namespace TravelAccounting.Model.Xml
-{
-    public class Serializer : ISerializer
-    {
-        public XDocument Serialize(Travel travel)
-        {
+namespace TravelAccounting.Model.Xml {
+    public class Serializer : ISerializer {
+        public XDocument Serialize(Travel travel) {
             var accountSet = createAccountSet(travel);
             var transactionSet = createTransactionSet(travel);
 
@@ -27,7 +24,12 @@ namespace TravelAccounting.Model.Xml
                 new XElement("Transaction",
                     new XElement("Details", t.Details),
                     new XElement("Id", XmlConvert.ToString(transactionSet[t])),
-                    new XElement("Date", XmlConvert.ToString(t.Date, XmlDateTimeSerializationMode.Utc))
+                    new XElement("Date", XmlConvert.ToString(t.Date, XmlDateTimeSerializationMode.Utc)),
+                    new XElement("Currency",
+                        new XElement("Name", t.Currency.Name),
+                        new XElement("ShortName", t.Currency.ShortName),
+                        new XElement("ExchangeRate", XmlConvert.ToString(t.Currency.ExchangeRate))
+                    )
                 )
             ).ToArray();
 
@@ -36,7 +38,7 @@ namespace TravelAccounting.Model.Xml
                     new XElement("Creditor", XmlConvert.ToString(accountSet[t.Creditor])),
                     new XElement("Debtor", XmlConvert.ToString(t.Debtor == null ? Guid.Empty : accountSet[t.Debtor])),
                     new XElement("Transaction", XmlConvert.ToString(transactionSet[t.Transaction])),
-                    new XElement("Amount", XmlConvert.ToString(t.BaseAmount)),
+                    new XElement("ActualAmount", XmlConvert.ToString(t.ActualAmount)),
                     new XElement("Date", XmlConvert.ToString(t.Date, XmlDateTimeSerializationMode.Utc))
                 )
             ).ToArray();
@@ -45,7 +47,19 @@ namespace TravelAccounting.Model.Xml
                 new XElement("Travel",
                     new XElement("Name", travel.Name),
                     new XElement("Details", travel.Details),
+                    new XElement("BaseCurrency",
+                        new XElement("Name", travel.BaseCurrency.Name),
+                        new XElement("ShortName", travel.BaseCurrency.ShortName)
+                    ),
                     new XElement("Accounts", accounts),
+                    new XElement("Currencies", travel.Currencies.Select(c =>
+                        new XElement("Currency",
+                            new XElement("Name", c.Name),
+                            new XElement("ShortName", c.ShortName),
+                            new XElement("ExchangeRate", XmlConvert.ToString(c.ExchangeRate))
+                            )
+                        ).ToArray()
+                    ),
                     new XElement("Transactions", transactions),
                     new XElement("TransactionLines", transactionLines)
                 )
@@ -53,13 +67,23 @@ namespace TravelAccounting.Model.Xml
             return doc;
         }
 
-        public Travel Deserialize(XDocument xDoc)
-        {
-            var travel = new Travel()
-            {
+        public Travel Deserialize(XDocument xDoc) {
+            var travel = new Travel() {
                 Name = xDoc.Root.Element("Name").Value,
-                Details = xDoc.Root.Element("Details").Value
+                Details = xDoc.Root.Element("Details").Value,
             };
+            travel.BaseCurrency.Name = xDoc.Root.Element("BaseCurrency").Element("Name").Value;
+            travel.BaseCurrency.ShortName = xDoc.Root.Element("BaseCurrency").Element("ShortName").Value;
+
+            foreach (var x in xDoc.Root.Element("Currencies").Elements("Currency")) {
+                travel.Currencies.Add(
+                    new Currency() {
+                        Name = x.Element("Name").Value,
+                        ShortName = x.Element("ShortName").Value,
+                        ExchangeRate = XmlConvert.ToDecimal(x.Element("ExchangeRate").Value)
+                    }
+                );
+            }
 
             var accountSet = createAccountSet(xDoc, travel);
             var transactionSet = createTransactionSet(xDoc, travel);
@@ -70,8 +94,7 @@ namespace TravelAccounting.Model.Xml
             foreach (var t in transactionSet.Values)
                 travel.Transactions.Add(t);
 
-            foreach (var x in xDoc.Root.Element("TransactionLines").Elements("TransactionLine"))
-            {
+            foreach (var x in xDoc.Root.Element("TransactionLines").Elements("TransactionLine")) {
                 var creditorId = XmlConvert.ToGuid(x.Element("Creditor").Value);
                 var debtorId = XmlConvert.ToGuid(x.Element("Debtor").Value);
                 var transId = XmlConvert.ToGuid(x.Element("Transaction").Value);
@@ -82,47 +105,44 @@ namespace TravelAccounting.Model.Xml
                 if (debtorId != Guid.Empty)
                     debtor = accountSet[debtorId];
 
-
-                //travel.TransactionLines.Add(
-                //    new TransactionLine(trans)
-                //    {
-                //        BaseAmount = XmlConvert.ToDecimal(x.Element("Amount").Value),
-                //        Creditor = creditor,
-                //        Debtor = debtor,
-                //        Date = XmlConvert.ToDateTime(x.Element("Date").Value, XmlDateTimeSerializationMode.Utc)
-                //    }
-                //);
+                travel.TransactionLines.Add(
+                    new TransactionLine(trans) {
+                        ActualAmount = XmlConvert.ToDecimal(x.Element("ActualAmount").Value),
+                        Creditor = creditor,
+                        Debtor = debtor,
+                        Date = XmlConvert.ToDateTime(x.Element("Date").Value, XmlDateTimeSerializationMode.Utc)
+                    }
+                );
             }
 
             return travel;
         }
 
-        private static Dictionary<Guid, Transaction> createTransactionSet(XDocument xDoc, Travel travel)
-        {
+        private static Dictionary<Guid, Transaction> createTransactionSet(XDocument xDoc, Travel travel) {
             var transactionSet = new Dictionary<Guid, Transaction>();
-            foreach (var x in xDoc.Root.Element("Transactions").Elements("Transaction"))
-            {
+            foreach (var x in xDoc.Root.Element("Transactions").Elements("Transaction")) {
                 transactionSet.Add(
                     XmlConvert.ToGuid(x.Element("Id").Value),
-                    new Transaction(travel)
-                    {
+                    new Transaction(travel) {
                         Details = x.Element("Details").Value,
-                        Date = XmlConvert.ToDateTime(x.Element("Date").Value, XmlDateTimeSerializationMode.Utc)
+                        Date = XmlConvert.ToDateTime(x.Element("Date").Value, XmlDateTimeSerializationMode.Utc),
+                        Currency = new Currency() {
+                            Name = x.Element("Currency").Element("Name").Value,
+                            ShortName = x.Element("Currency").Element("ShortName").Value,
+                            ExchangeRate = XmlConvert.ToDecimal(x.Element("Currency").Element("ExchangeRate").Value)
+                        }
                     }
                 );
             }
             return transactionSet;
         }
 
-        private static Dictionary<Guid, Account> createAccountSet(XDocument xDoc, Travel travel)
-        {
+        private static Dictionary<Guid, Account> createAccountSet(XDocument xDoc, Travel travel) {
             var accountSet = new Dictionary<Guid, Account>();
-            foreach (var x in xDoc.Root.Element("Accounts").Elements("Account"))
-            {
+            foreach (var x in xDoc.Root.Element("Accounts").Elements("Account")) {
                 accountSet.Add(
                     XmlConvert.ToGuid(x.Element("Id").Value),
-                    new Account(travel)
-                    {
+                    new Account(travel) {
                         Name = x.Element("Name").Value
                     }
                 );
@@ -130,16 +150,14 @@ namespace TravelAccounting.Model.Xml
             return accountSet;
         }
 
-        private static Dictionary<Transaction, Guid> createTransactionSet(Travel travel)
-        {
+        private static Dictionary<Transaction, Guid> createTransactionSet(Travel travel) {
             var transactionSet = new Dictionary<Transaction, Guid>();
             foreach (var t in travel.Transactions)
                 transactionSet.Add(t, Guid.NewGuid());
             return transactionSet;
         }
 
-        private static Dictionary<Account, Guid> createAccountSet(Travel travel)
-        {
+        private static Dictionary<Account, Guid> createAccountSet(Travel travel) {
             var accountSet = new Dictionary<Account, Guid>();
             foreach (var a in travel.Accounts)
                 accountSet.Add(a, Guid.NewGuid());
